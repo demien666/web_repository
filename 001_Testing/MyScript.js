2416dbfc652829f3bc40c27ca4f8b3cea245c525
Dmytro Kovalskyi <dmytro.kovalskyi@synapse.com>
2016-09-05 17:03:06 +0200

    001_Testing short form of way finding

--- 001_Testing/MyScript.js
+++ 001_Testing/MyScript.js
@@ -1,10 +1,10 @@
 /*jslint browser: true*/
 /*global console*/
 
+'use strict';
 var isTraceEnabled = true;
 
 function getLogger() {
-    'use strict';
     var self = Object.create(null);
 
     self.trace = function (msg) {
@@ -18,33 +18,68 @@ function getLogger() {
 var logger = getLogger();
 
 function Point(x, y) {
-    "use strict";
-	var self = Object.create(null);
+    var self = Object.create(null);
     self.x = x;
     self.y = y;
     self.equals = function (anotherPoint) {
         return (self.x === anotherPoint.x && self.y === anotherPoint.y);
     };
-	return self;
+    return self;
 }
 
 function isValidPosition(point, map) {
-    'use strict';
     var x = point.x;
     var y = point.y;
-    logger.trace('checking [' + x + ',' + y + ']');
+    //logger.trace('    checking isValidPosition: [' + x + ',' + y + ']');
     return (x >= 0 && x < Object.keys(map).length && y >= 0 && y < map[0].length);
 }
 
 function isVisited(point, visited) {
-    'use strict';
-    return visited[point.x][point.y] == null;
+    //logger.trace('    checking isVisited: [' + point.x + ',' + point.y + ']');
+    return visited[point.x][point.y] == 1;
 }
 
-function getPositionsAround(point) {
-    'use strict';
+function isAcessable(point, map) {
+    //logger.trace('    checking isAcessable: [' + point.x + ',' + point.y + ']');
+    return map[point.x][point.y] == 1;
+}
+
+function getPointsAround(point) {
     var x = point.x;
     var y = point.y;
-    //logger.trace('positions around [' + x + ',' + y + ']');
     return [new Point(x - 1, y), new Point(x + 1, y), new Point(x, y - 1), new Point(x, y + 1)];
+}
+
+function getNextPoints(point, map, visited) {
+    //logger.trace('  getNextPoints: [' + point.x + ',' + point.y + ']');
+    var nexts = getPointsAround(point);
+    var result = [];
+    nexts.map(function (next) {
+        if (isValidPosition(next, map) && isAcessable(next, map) && !isVisited(next, visited)) {
+            result.push(next);
+        }
+    });
+    return result;
+}
+
+function go(startPoint, endPoint, map, visited) {
+    var queue = [];
+    var current;
+    var nexts;
+
+    queue.push(startPoint);
+    while (queue.length > 0) {
+        current = queue.shift();
+        logger.trace('processing from queue: [' + current.x + ',' + current.y + ']');
+        if (current.equals(endPoint)) {
+            return current;
+        }
+        visited[current.x][current.y] = 1;
+
+        nexts = getNextPoints(current, map, visited);
+
+        nexts.map(function (next) {
+            queue.push(next);
+        });
+    }
 }
--- 001_Testing/MyScriptTest.js
+++ 001_Testing/MyScriptTest.js
@@ -1,15 +1,35 @@
-/*global describe, assert, it, logger, Point, getPositionsAround*/
+/*global beforeEach, describe, assert, it, logger, Point, isValidPosition, getPointsAround, getNextPoints, go*/
 describe("Main test", function () {
     "use strict";
 
+    var map;
+    var visited;
+
+    beforeEach(function () {
+        map = [
+            [1, 0, 0, 0, 0],
+            [1, 0, 1, 1, 0],
+            [1, 1, 1, 1, 0],
+            [1, 0, 0, 1, 0],
+            [1, 0, 0, 1, 1]
+        ];
+        visited = [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0]
+        ];
+    });
+
     describe("Point class", function () {
-        it("EQUALS function for another point with the same coordinates should return TRUE", function () {
+        it("should return TRUE on calling EQUALS for another point with the same coordinates", function () {
             var point = new Point(1, 2);
             var theSamePoint = new Point(1, 2);
             assert.equal(point.equals(theSamePoint), true);
         });
 
-        it("EQUALS function for another point with the different coordinates should return FALSE", function () {
+        it("should return FALSE on calling EQUALS for another point with the different coordinates", function () {
             var point = new Point(1, 2);
             var anotherPoint = new Point(1, 3);
             assert.equal(point.equals(anotherPoint), false);
@@ -17,30 +37,87 @@ describe("Main test", function () {
 
     });
 
-    describe("getNextPossiblePositions", function () {
+    describe("isValidPosition", function () {
+        it("should be TRUE for coordinates inside map", function () {
+            assert.equal(isValidPosition(new Point(1, 1), map), true);
+        });
+
+        it("should be FALSE for coordinates with x<0", function () {
+            assert.equal(isValidPosition(new Point(-1, 1), map), false);
+        });
+
+        it("should be FALSE for coordinates with x>=map.length", function () {
+            assert.equal(isValidPosition(new Point(Object.keys(map).length, 1), map), false);
+        });
+
+        it("should be FALSE for coordinates with y<0", function () {
+            assert.equal(isValidPosition(new Point(1, -1), map), false);
+        });
+
+        it("should be FALSE for coordinates with y>=map.length", function () {
+            assert.equal(isValidPosition(new Point(1, map[0].length), map), false);
+        });
+
+    });
+
+    describe("getPointsAround", function () {
         var x = 1;
         var y = 1;
         var point = new Point(x, y);
-        var positions = getPositionsAround(point);
+        var positions = getPointsAround(point);
 
         it("Should return 4 positions around point", function () {
             assert.equal(positions.length, 4);
         });
 
-        it("Position #1 should be [x-1,y]", function () {
+        it("Should be [x-1,y] for position #1", function () {
             assert.equal(positions[0].equals(new Point(x - 1, y)), true);
         });
 
-        it("Position #2 should be [x+1,y]", function () {
+        it("Should be [x+1,y] for position #2 ", function () {
             assert.equal(positions[1].equals(new Point(x + 1, y)), true);
         });
 
-        it("Position #3 should be [x,y-1]", function () {
+        it("Should be [x,y-1] for position #3 ", function () {
             assert.equal(positions[2].equals(new Point(x, y - 1)), true);
         });
 
-        it("Position #4 should be [x,y+1]", function () {
+        it("Should be [x,y+1] for position #4 ", function () {
             assert.equal(positions[3].equals(new Point(x, y + 1)), true);
+        });
+
+    });
+
+    describe("getNextPoints", function () {
+        var nexts;
+        it("Should return 1 next point from position [0,0]", function () {
+            nexts = getNextPoints(new Point(0, 0), map, visited);
+            assert.equal(nexts.length, 1);
+            assert.equal(nexts[0].equals(new Point(1, 0)), true);
+        });
+
+        it("Should return 2 next points from position [2,1]", function () {
+            nexts = getNextPoints(new Point(2, 1), map, visited);
+            assert.equal(nexts.length, 2);
+            assert.equal(nexts[0].equals(new Point(2, 0)), true);
+            assert.equal(nexts[1].equals(new Point(2, 2)), true);
+        });
+
+        it("Should return 1 next point from position [2,1] if position [2,0] was visited", function () {
+            visited[2][0] = 1;
+            nexts = getNextPoints(new Point(2, 1), map, visited);
+            assert.equal(nexts.length, 1);
+            assert.equal(nexts[0].equals(new Point(2, 2)), true);
+        });
+    });
+
+    describe("do", function () {
+        var startPoint = new Point(0, 0);
+        var endPoint = new Point(4, 4);
+
+        it("Should return endPoint for valid parameters", function () {
+            var result = go(startPoint, endPoint, map, visited);
+            assert.equal(result.equals(endPoint), true);
         });
 
     });

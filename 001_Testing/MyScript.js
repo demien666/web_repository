0a09466ab2225dbea444882c46ba5195d51897f6
Dmytro Kovalskyi <dmytro.kovalskyi@synapse.com>
2016-09-05 13:14:44 +0200

    001_Testing added test for getNextPossiblePositions

--- 001_Testing/MyScript.js
+++ 001_Testing/MyScript.js
@@ -1,8 +1,50 @@
-function Point(x, y) {
-  this.x=x;
-  this.y=y;
+/*jslint browser: true*/
+/*global console*/
 
-  this.equals=function(anotherPoint) {
-    return (this.x===anotherPoint.x && this.y===anotherPoint.y);
-  }
+var isTraceEnabled = true;
+
+function getLogger() {
+    'use strict';
+    var self = Object.create(null);
+
+    self.trace = function (msg) {
+        if (isTraceEnabled) {
+            console.log(msg);
+        }
+    };
+    return self;
+}
+
+var logger = getLogger();
+
+function Point(x, y) {
+    "use strict";
+	var self = Object.create(null);
+    self.x = x;
+    self.y = y;
+    self.equals = function (anotherPoint) {
+        return (self.x === anotherPoint.x && self.y === anotherPoint.y);
+    };
+	return self;
+}
+
+function isValidPosition(point, map) {
+    'use strict';
+    var x = point.x;
+    var y = point.y;
+    logger.trace('checking [' + x + ',' + y + ']');
+    return (x >= 0 && x < Object.keys(map).length && y >= 0 && y < map[0].length);
+}
+
+function isVisited(point, visited) {
+    'use strict';
+    return visited[point.x][point.y] == null;
+}
+
+function getPositionsAround(point) {
+    'use strict';
+    var x = point.x;
+    var y = point.y;
+    //logger.trace('positions around [' + x + ',' + y + ']');
+    return [new Point(x - 1, y), new Point(x + 1, y), new Point(x, y - 1), new Point(x, y + 1)];
 }
--- 001_Testing/MyScriptTest.js
+++ 001_Testing/MyScriptTest.js
@@ -1,15 +1,48 @@
-describe("Point class", function() {
+/*global describe, assert, it, logger, Point, getPositionsAround*/
+describe("Main test", function () {
+    "use strict";
 
-  it("EQUALS function for another point with the same coordinates should return TRUE", function() {
-    var point=new Point(1,2);
-    var theSamePoint=new Point(1,2);
-    assert.equal(point.equals(theSamePoint), true);
-  });
+    describe("Point class", function () {
+        it("EQUALS function for another point with the same coordinates should return TRUE", function () {
+            var point = new Point(1, 2);
+            var theSamePoint = new Point(1, 2);
+            assert.equal(point.equals(theSamePoint), true);
+        });
 
-  it("EQUALS function for another point with the different coordinates should return FALSE", function() {
-    var point=new Point(1,2);
-    var anotherPoint=new Point(1,3);
-    assert.equal(point.equals(anotherPoint), false);
-  });
+        it("EQUALS function for another point with the different coordinates should return FALSE", function () {
+            var point = new Point(1, 2);
+            var anotherPoint = new Point(1, 3);
+            assert.equal(point.equals(anotherPoint), false);
+        });
+
+    });
+
+    describe("getNextPossiblePositions", function () {
+        var x = 1;
+        var y = 1;
+        var point = new Point(x, y);
+        var positions = getPositionsAround(point);
+
+        it("Should return 4 positions around point", function () {
+            assert.equal(positions.length, 4);
+        });
+
+        it("Position #1 should be [x-1,y]", function () {
+            assert.equal(positions[0].equals(new Point(x - 1, y)), true);
+        });
+
+        it("Position #2 should be [x+1,y]", function () {
+            assert.equal(positions[1].equals(new Point(x + 1, y)), true);
+        });
+
+        it("Position #3 should be [x,y-1]", function () {
+            assert.equal(positions[2].equals(new Point(x, y - 1)), true);
+        });
+
+        it("Position #4 should be [x,y+1]", function () {
+            assert.equal(positions[3].equals(new Point(x, y + 1)), true);
+        });
+
+    });
 
 });

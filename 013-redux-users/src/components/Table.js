44e4d4237f8c6c785f6e231649e2ba9b76a26190
Dmitry Kovalsky <dmitry.n.kovalsky@gmail.com>
2017-08-13 15:15:55 +0200

    013-redux-users table component is working

--- 013-redux-users/src/components/Table.js
+++ 013-redux-users/src/components/Table.js
@@ -1,9 +1,25 @@
 import	React,	{	PropTypes,	Component	}	from	'react'
 
+const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 const renderTableRow = (columns, data) => {
-    let rowContent = columns.map((column) =>
+    let rowContent = [];
+    columns.map( (column) => {
+        let value =  data[column.name];
+        if (value === undefined) {
+            const getMethodName = "get"+capitalizeFirstLetter(column.name);
+            value = data[getMethodName].apply(data);
+        }
+        rowContent.push(<td key={column.name + "-" + data.id}>{value}</td>);
+     
+    });
+    /*
+    let rowContent = columns.map((column) =>       
         <td key={column.name + "-" + data.id}>{data[column.name]}</td>
     );
+    */
     return (<tr key={data.id}>{rowContent}</tr>);
 };
 

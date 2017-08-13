d19b4001f505c24f12d420764a442f2fe279fc76
Dmitry Kovalsky <dmitry.n.kovalsky@gmail.com>
2017-08-13 21:45:20 +0200

    013-redux-users table component

--- 013-redux-users/src/components/Table.js
+++ 013-redux-users/src/components/Table.js
@@ -1,25 +1,25 @@
-import	React,	{	PropTypes,	Component	}	from	'react'
+import React from 'react'
 
 const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
-
-const renderTableRow = (columns, data) => {
-    let rowContent = [];
-    columns.map( (column) => {
-        let value =  data[column.name];
-        if (value === undefined) {
-            const getMethodName = "get"+capitalizeFirstLetter(column.name);
-            value = data[getMethodName].apply(data);
-        }
-        rowContent.push(<td key={column.name + "-" + data.id}>{value}</td>);
-     
-    });
-    /*
+/*
     let rowContent = columns.map((column) =>       
         <td key={column.name + "-" + data.id}>{data[column.name]}</td>
     );
     */
+const renderTableRow = (columns, data) => {
+    let rowContent = [];
+    columns.map((column) => {
+        let value = data[column.name];
+        if (value === undefined) {
+            const getMethodName = "get" + capitalizeFirstLetter(column.name);
+            value = data[getMethodName].apply(data);
+        }
+        rowContent.push(<td key={column.name + "-" + data.id}>{value}</td>);
+        return null;
+    });
+    
     return (<tr key={data.id}>{rowContent}</tr>);
 };
 
@@ -33,20 +33,20 @@ const renderTableRows = (columns, objectList) => (
     objectList.map((data) => renderTableRow(columns, data))
 );
 
-export function renderTable (objectType, columns, objectList) {
+export function renderTable(objectType, columns, objectList) {
     if (!objectList || objectList.length === 0)
         return null;
-    return(
-            <div>
-                <h3>List of existing:</h3>
-                <table id={"table-"+objectType} className="objectListTable">
-                    <tbody>
-                        <tr>
-                            {renderTableHeader(columns)}
-                        </tr>               
-                        {renderTableRows(columns, objectList)}
-                    </tbody>        
-                </table>
-            </div>
+    return (
+        <div>
+            <h3>List of existing:</h3>
+            <table id={"table-" + objectType} className="objectListTable">
+                <tbody>
+                    <tr>
+                        {renderTableHeader(columns)}
+                    </tr>
+                    {renderTableRows(columns, objectList)}
+                </tbody>
+            </table>
+        </div>
     );
 };
